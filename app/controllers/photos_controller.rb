2e94b51a0065ebbff530a9966e668189b143c73c
natashasingh2018 <natashasingh2018@users.noreply.github.com>
2017-04-25 09:40:11 -0500

    show page

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,20 @@
+class PhotosController < ApplicationController
+
+  def show
+    #params hash looks like {"the_id_number"=>"1"}
+
+    id_number = params["the_id_number"]
+    p = Photo.find(id_number)
+
+    @the_caption = p.caption
+
+    @the_image_url = p.source
+
+    @created_at_time = p.created_at
+
+    render("show.html.erb")
+
+  end
+
+
+end
