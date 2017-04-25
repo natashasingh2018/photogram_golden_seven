c2f37cafd5336cffaa69d0c79bc592b2ae629a33
natashasingh2018 <natashasingh2018@users.noreply.github.com>
2017-04-25 16:42:40 -0500

    updated all, need to test this version

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
 
+  #CREATE
+  get("/photos/new", {:controller=>"photos", :action=>"new_form"})
+  get("/create_photo", {:controller=>"photos", :action=>"create_row"})
 
-get("/photos/:the_id_number", {:controller=> "photos", :action=>"show"})
+  #READ
+  get("/photos/:the_id_number", {:controller=> "photos", :action=>"show"})
+  get("/photos", {:controller=> "photos", :action=>"index"})
 
-get("/photos", {:controller=> "photos", :action=>"index"})
+  #UPDATE
+  get("/photos/:the_id/edit", {:controller=> "photos", :action=>"edit_form"})
+  get("/update_photo/:the_id", {:controller=> "photos", :action=>"update_row"})
+
+  #DELETE
+  get("/photos/:the_id/delete", {:controller=> "photos", :action=>"delete_row"})
+
 
 
   devise_for :admin_users, ActiveAdmin::Devise.config
